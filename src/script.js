0db2d4126f7022fae37ffa9051558c857e0c51d5
Bridgett76 <louniegirl@gmail.com>
2024-08-21 21:40:00 -0500

    Changed the api context

--- src/script.js
+++ src/script.js
@@ -13,7 +13,7 @@ function generatePoem(event) {
   let apiKey = "75446f1f7d0612tcbfa113fo7a796523";
   let prompt = `User instructions: Generate a nature poem about ${topicInput.value}`;
   let context =
-    "You are a creative Poem expert and love to write short poems. Your mission is to make a 6 line poem and separate each line with a </ br>. Please make sure to follow the user in structions.";
+    "You are a creative Poem expert and love to write short poems. Your mission is to make a 6 line poem and separate each line. Please make sure to follow the user in structions.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   axios.get(apiUrl).then(displayPoem);
